5bd52ccec14228e006981a982be10f1d1253824b
muumiso <38120187+muumiso@users.noreply.github.com>
2018-05-24 13:06:59 +0900

    完成

--- src/web/WCDBServlet.java
+++ src/web/WCDBServlet.java
@@ -1,6 +1,12 @@
 package web;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -27,12 +33,69 @@ public class WCDBServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setAttribute("kaiinnNo", Integer.toString(1));
-		request.setAttribute("kaiinnName", "中村");
-		request.setAttribute("registDate", "2018/05/24");
+		String kaiinnNostr = request.getParameter("kaiinnNo");
 
-		RequestDispatcher disp = request.getRequestDispatcher("/serch.jsp");
-		disp.forward(request, response);
+		PreparedStatement stmt = null;
+		ResultSet rset = null;
+
+		try {
+			//接続文字列の構築
+			/* ユーザ名 */
+			String user = "root";
+			/* パスワード */
+			String pass = "root";
+
+			/* サーバ名 */
+			String servername = "localhost:3306";
+			/* DB名 */
+			String dbname = "new_schema";
+
+			// ドライバーのロード
+			Class.forName("com.mysql.jdbc.Driver");
+
+			//接続の実行とコネクションオブジェクトの取得
+			Connection c = DriverManager.getConnection(
+					"jdbc:mysql://"
+							+ servername
+							+ "/"
+							+ dbname,
+					user,
+					pass);
+
+			/* Statementの作成 */
+			stmt = c.prepareStatement(
+					"select "
+							+ "   kaiinNo"
+							+ "  ,name"
+							+ "  ,registDate"
+							+ " from "
+							+ "   kaiinn "
+							+ "where "
+							+ "  kaiinNo =? ");
+
+			stmt.setInt(1, Integer.parseInt(kaiinnNostr));
+
+			/* ｓｑｌ実行 */
+			rset = stmt.executeQuery();
+
+			String kaiinnName = null;
+			LocalDate registDate = null;
+
+			/* 取得したデータを表示します。 */
+			while (rset.next()) {
+				kaiinnName = rset.getString(2);
+				registDate = rset.getDate(3).toLocalDate();
+			}
+
+			request.setAttribute("kaiinnNo", kaiinnNostr);
+			request.setAttribute("kaiinnName", kaiinnName);
+			request.setAttribute("registDate", registDate.toString());
+
+			RequestDispatcher disp = request.getRequestDispatcher("/serch.jsp");
+			disp.forward(request, response);
+		} catch (ClassNotFoundException | SQLException e) {
+			throw new ServletException(e);
+		}
 	}
 
 }
