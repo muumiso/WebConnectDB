85a61580fe6537076ac4da552e0f857efc4af3ad
muumiso <38120187+muumiso@users.noreply.github.com>
2018-05-24 11:15:24 +0900

    基本設計

---
+++ src/web/WCDBServlet.java
@@ -0,0 +1,34 @@
+package web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WCDBServlet
+ */
+@WebServlet("/WCDBServlet")
+public class WCDBServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WCDBServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher disp = request.getRequestDispatcher("/serch.jsp");
+		disp.forward(request, response);
+	}
+
+}
